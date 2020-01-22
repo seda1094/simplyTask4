8d3d8c5c42411727971ffa83f73d690ac394a361
Seda Tathevik Hayrapetyan <sedahayrapetyan@Sedas-MacBook-Pro.local>
2020-01-23 00:18:32 +0400

    gladiator class is created

---
+++ gladiator.js
@@ -0,0 +1,8 @@
+class Gladiator {
+    constructor(name,health, power, speed){
+        this.name = name;
+        this.health = health;
+        this.power = power;
+        this.speed = speed;
+    }
+}
