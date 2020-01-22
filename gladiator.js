bef3a3b8ccc28c2ee63cd9d7bd38c4edc88d5256
Seda Tathevik Hayrapetyan <sedahayrapetyan@Sedas-MacBook-Pro.local>
2020-01-23 00:49:58 +0400

    created getAllGladiatorLocations and chooseRndGladiator functions

--- gladiator.js
+++ gladiator.js
@@ -1,8 +1,30 @@
 class Gladiator {
-    constructor(name,health, power, speed){
+    constructor(x, y, name, health, power, speed) {
+        this.x = x;
+        this.y = y;
         this.name = name;
         this.health = health;
         this.power = power;
         this.speed = speed;
+    }
+    getAllGladiatorLocations(character) {
+        const found = [];
+        for (var y = 0; y < matrix.length; y++) {
+            for (var x = 0; x < matrix[y].length; x++) {
+     
+                if (matrix[y][x] == 1) {
+                    found.push({y: y, x: x})
+                }
+            }
+        }
+        return found;
+    }
+    chooseRndGladiator(){
+        const gladiatorLocations = this.getAllGladiatorLocations(1);
+        const rndGladiator = gladiatorLocations[Math.floor(Math.random()*gladiatorLocations.length)]
+        console.log(rndGladiator);
+    }
+    hit() {
+
     }
 }
